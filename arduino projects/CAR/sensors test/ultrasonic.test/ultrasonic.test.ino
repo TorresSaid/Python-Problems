fa7f8c381510cd82e5e02489c3c0216a6913140e
TorresSaid <fatm479@gmail.com>
2026-06-21 19:31:57 -0500

    cambios brutales

--- arduino projects/CAR/sensors test/ultrasonic.test/ultrasonic.test.ino
+++ arduino projects/CAR/sensors test/ultrasonic.test/ultrasonic.test.ino
@@ -10,17 +10,17 @@ void setup() {
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  
+
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   long duration = pulseIn(echoPin, HIGH);
   float distance = duration * 0.034 / 2;
-  
+
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  
+
   delay(500);
 }
